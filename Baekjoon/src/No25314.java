759f737667190023fdccf26ff67073ae82e4e97d
soon1541 <rkqkrdlans123@naver.com>
2023-07-25 21:37:58 +0900

    2023 07 25

---
+++ Baekjoon/src/No25314.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class No25314 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		String s = "";
+		
+		if(n > 4) {
+		for (int i = 8; i <= n; i+=4) {
+			s += "long";
+		}
+	
+	
+	}
+		s += "long int";
+		System.out.println(s);
+	
+	
+	
+	
+	}
+
+}
