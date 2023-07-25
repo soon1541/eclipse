b89526ac91c97918214e3222bc4f3915d282a51a
soon1541 <rkqkrdlans123@naver.com>
2023-07-25 21:35:56 +0900

    2023 07 25

---
+++ Baekjoon/src/No10950.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class No10950 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		
+		int A = scan.nextInt();
+		
+		for (int i = 0; i < A; i++) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			System.out.println(a+b);
+		}	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
