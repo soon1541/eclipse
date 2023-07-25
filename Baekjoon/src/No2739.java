e65d97258ade91915429d20d27482929c21db597
soon1541 <rkqkrdlans123@naver.com>
2023-07-25 21:38:27 +0900

    2023 07 25

---
+++ Baekjoon/src/No2739.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class No2739 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		
+		int a = scan.nextInt();
+		
+		
+		for (int i = 1; i <=9; i++) {
+			int result = (a*i); 
+			System.out.println(a+" * "+i+" = "+result);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
