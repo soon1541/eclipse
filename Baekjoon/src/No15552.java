785ed35536ad46be7c76e011fa280f08cfabdfc6
soon1541 <rkqkrdlans123@naver.com>
2023-07-26 21:24:28 +0900

    2023 07 26

---
+++ Baekjoon/src/No15552.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No15552 {
+
+	public static void main(String[] args) throws IOException {
+		
+		//선언
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		// 형변환
+		int T = Integer.parseInt(br.readLine());
+		
+		// readLine 입력값을 공백단위로 구분
+		StringTokenizer st;
+		
+		//입력
+			for(int i = 0; i < T; i++) {
+				st = new StringTokenizer(br.readLine());
+			
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
