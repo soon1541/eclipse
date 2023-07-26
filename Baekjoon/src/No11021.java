aa0178b052cf582a47f595bb34a0f29eb4515726
soon1541 <rkqkrdlans123@naver.com>
2023-07-26 21:24:53 +0900

    2023 07 26

---
+++ Baekjoon/src/No11021.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class No11021 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		int T = scan.nextInt();
+		
+		
+		
+		for(int i = 1; i <= T; i++) {
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+			int sum = A+B;
+			System.out.println("Case #" + i +  ": " + sum);
+			}
+		
+	}
+
+}
