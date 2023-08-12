6fe52f316052a88a1d714b8a3b606454b55cb38e
soon1541 <rkqkrdlans123@naver.com>
2023-08-12 21:35:38 +0900

    2023 08 12

---
+++ java11_net/src/java11_net/UnicasteSend.java
@@ -0,0 +1,42 @@
+package java11_net;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UnicasteSend {
+
+	public UnicasteSend() {
+		
+	}
+	public void sendStart() {
+		try {
+			String data = "java 네트워크 프로그래밍";
+			
+			DatagramSocket ds = new DatagramSocket();
+			InetAddress ia = InetAddress.getByName("182.237.244.84");
+			
+			DatagramPacket dp = new DatagramPacket(data.getBytes(), data.getBytes().length, ia, 9999);
+			ds.send(dp);
+			System.out.println("보내기 완료");
+			
+			
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		new UnicasteSend().sendStart();
+
+	}
+
+}
